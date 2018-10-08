801146b216fb6255fffed042bafd0f83af655409
DK\IEKA <ieka@sitecore.net>
2018-10-08 11:33:35 +0300

    Fix issue when "Sitecore.Abstractions.BaseCorePipelineManager" service was not registered in container.

---
+++ src/Sitecore.Support.257345/Pipelines/Loader/RegisterServices.cs
@@ -0,0 +1,15 @@
+﻿using Sitecore.Abstractions;
+using Sitecore.ContentSearch;
+using Sitecore.DependencyInjection;
+using Sitecore.Pipelines;
+
+namespace Sitecore.Support.Pipelines.Loader
+{
+  public class RegisterServices
+  {
+    public void Process(PipelineArgs args)
+    {
+      ContentSearchManager.Locator.Register(c => ServiceLocator.ServiceProvider.GetService(typeof(BaseCorePipelineManager)) as BaseCorePipelineManager);
+    }
+  }
+}
---
+++ src/Sitecore.Support.257345/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.257345")]
+[assembly: AssemblyProduct("Sitecore.Support.257345")]
+[assembly: ComVisible(false)]
